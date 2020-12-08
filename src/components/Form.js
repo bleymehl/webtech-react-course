c1b6bfd66b92bbf49220e65add98e19128f616a3
Steve Rehm <steve.rehm@599media.de>
2020-12-08 16:09:52 +0100

    [master] #CHANGE 'scope: form' <master>
    dokumentation im quellcode

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,15 @@
+/**
+ * Import von React und allen relevanten React-Komponenten
+ */
 import React, { useEffect, useState, useRef, useReducer } from "react"
 
 function Form() {
+  /**
+   * Der useRef-Hook gibt ein veränderbares ref-Objekt zurück,
+   * dessen Eigenschaft .current auf das übergebene Argument (initialValue) initialisiert ist.
+   * Das zurückgegebene Objekt bleibt für die gesamte Lebensdauer der Komponente bestehen.
+   * Beispiel: <input ref={formCheck} />
+   */
   const formInput = useRef()
   const formCheck = useRef()
 
@@ -14,9 +23,14 @@ function Form() {
   /**
    * TODO:
    * initial state object "originalState" definieren
-   * email (value:string, hasErrors: boolean, message:string)
+   * email:string(null), error: boolean(false), success:boolean(false)
+   *
+   * email: Input, der über das Input-Feld in der Form generiert wird
+   * error: abhängig von der Validierung der E-Mail-Adresse
+   * success: abhängig davon, ob die Checkbox checked ist und es keine Fehler gibt.
+   *
+   * Der success state steuert außerdem die Anzeige der Meldung nach erfolgreichem Submit.
    */
-
   const originalState = {
     email: null,
     error: false,
@@ -24,35 +38,77 @@ function Form() {
   }
 
   /**
-   * Reducer
-   * Ein Reducer ist eine Funktion, die zwei Argumente benötigt - den aktuellen Zustand und eine Aktion - und auf der Grundlage beider Argumente einen neuen Zustand zurückgibt.
+   * Der useReducer-Hook ist eine Alternative zur Verwendung useState.
+   * Er akzeptiert einen reducer vom Typ (state, action) => newState und
+   * gibt den aktuellen state gepaart mit einer Dispatch-Methode zurück.
+   * useReducer ist in der Regel bei komplexer Zustandslogik dem useState vorzuziehen.
    */
+  const [state, dispatch] = useReducer(ourReducer, originalState)
 
+  /**
+   * Ein Reducer benötigt zwei Argumente:
+   * - den aktuellen Zustand
+   * - eine Aktion
+   * Auf der Grundlage beider Argumente gibt er einen neuen Zustand zurück.
+   */
   function ourReducer(state, action) {
-    // eslint-disable-next-line default-case
     switch (action.type) {
+      /**
+       * Sobald sich der Value des E-Mail-Inputs ändert,
+       * wird der state email mit dem entsprechenden Input-Value aktualisiert.
+       * Zugleich wird der state success auf false gesetzt, damit die Meldung ausgeblendet wird.
+       */
       case "emailImmediately":
-        console.log(`Immediately: ${state.email}`)
         return {
+          /**
+           * Der Spread Operator kann verwendet werden, um ein vorhandenes Array zu nehmen
+           * und ihm ein weiteres Element hinzuzufügen, während das ursprüngliche Array erhalten bleibt.
+           * Wendet man den Spread Operator auf state mit ...state an, dann wird vom aktuellen state
+           * eine Kopie erstellt, die dann verändert werden kann.
+           * Damit wird der state nicht direkt manipuliert, so wie es auch die Philosophie von React vorsieht.
+           */
           ...state,
           email: action.value,
           success: false,
         }
+      /**
+       * Damit die eingebebe E-Mail nicht permanent bei jeder Änderung
+       * des Inputs neu geprüft wird, geschiet der Test mittels setTimeout-Function.
+       * Siehe useEffect-Hook um Zeile 100.
+       */
       case "emailAfterDelay":
-        console.log(`After Delay: ${state.email}`)
+        /**
+         * RegEx (regular expression) zum Abgleichen von E-Mail-Adressen
+         */
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         return {
           ...state,
+          /**
+           * Die JavaScript test-Function gibt true oder false,
+           * wenn der übergebene String dem Kriterium entspricht oder nicht.
+           * Die Konstante "re" ist das Kriterium.
+           */
           error: !re.test(String(state.email).toLowerCase()) ? true : false,
         }
+      /**
+       * Hier soll geprüft werden, ob die Checkbox checked ist
+       * und ob es keine Fehler bei der Eingabe der
+       * E-Mail-Adresse gibt. Wenn beide Kriterien erfüllt sind,
+       * wird der state success auf true gesetzt.
+       */
       case "submitForm":
-        console.log(`Submit: ${state.email}`)
         return {
           ...state,
           success: checked && !state.error ? true : false,
         }
+      /**
+       * Nach erfolgreicher Newsletter-Registrierung, soll die
+       * Form auf ihren Default-Zustand zurücksgesetzt werden.
+       * checked state: false
+       * checked-Attribut der Checkbox: false
+       * value des Input-Felds: ""
+       */
       case "resetForm":
-        console.log(`Reset: ${state.email}`)
         setChecked(false)
         formCheck.current.checked = false
         formInput.current.value = ""
@@ -64,11 +120,17 @@ function Form() {
     }
   }
 
-  const [state, dispatch] = useReducer(ourReducer, originalState)
-
   useEffect(() => {
     if (state.email) {
       const delay = setTimeout(() => dispatch({ type: "emailAfterDelay" }), 800)
+      /**
+       * Der useEffect-Hook ist so aufgebaut, dass man eine Funktion
+       * innerhalb der Methode zurückgeben (z.B. clearTimeout) kann.
+       * Diese Funktion wird ausgeführt, sobald die Komponente unmounted wird.
+       * Das ist sehr nützlich, da damit unnötiges Verhalten entfernt oder
+       * Probleme mit memory leaks verhindern werden können.
+       * Clean-Up-Functions gelten als best practice zur Perfomance-Optimierung.
+       */
       return () => clearTimeout(delay)
     }
   }, [state.email])
@@ -80,7 +142,14 @@ function Form() {
   }, [state.success])
 
   function submitHandler(e) {
+    /**
+     * Das Standardverhalten der Form soll deaktiviert werden.
+     */
     e.preventDefault()
+    /**
+     * Alle relevanten Dispatch-Types werden bei
+     * Klick auf den Submit-Button durchlaufen.
+     */
     dispatch({ type: "emailImmediately", value: state.email })
     dispatch({ type: "emailAfterDelay", value: state.email })
     dispatch({ type: "submitForm", value: state.email })
@@ -88,11 +157,18 @@ function Form() {
 
   return (
     <>
+      {/**
+       * Abhängig vom success state wird die Erfolgsmitteilung gerendert.
+       * Der Inhalt der Nachricht wird dynamisch durch email state generiert.
+       */}
       {state.success && (
         <div className="alert alert-success" role="alert">
           Sie habe sich erfolgreich mit <strong>{state.email}</strong> registriert.
         </div>
       )}
+      {/**
+       * Beim Submit der Form wird die submitHandler-Funktion aufgerufen.
+       */}
       <form onSubmit={submitHandler} className="my-3">
         <div className="row mb-2">
           <div className="col-sm-12 col-md-8">
@@ -113,6 +189,11 @@ function Form() {
             </div>
           </div>
         </div>
+        {/**
+         * TODO:
+         * In Abhängigkeit von dem checked state soll
+         * der Submit-Button dis- oder enabled werden.
+         */}
         <button type="submit" className="btn btn-primary btn-sm" disabled={!checked}>
           Absenden
         </button>
